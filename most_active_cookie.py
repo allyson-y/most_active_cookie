5d381e3ffdfd51de574c8d5f42c7cd9eb3ffba76
allyson-y <54675313+allyson-y@users.noreply.github.com>
2022-01-31 22:42:24 -0800

    final commit

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,34 +1,29 @@
 import csv, sys
 import os #library for checking if input file exists
 
-def main(argv):
-    if len(argv) != 3:
-        print("error: parameters not sufficient")
-        return
-    exists = os.path.exists(argv[0])
-    if exists:
-        filename = argv[0]
-    else:
+def most_active_cookie(filename, command, date):
+    """returns the most active cookies from the file for the input date; checks all the parameters"""
+    exists = os.path.exists(filename)
+    if not exists:
         print("error: file name doesn't exist")
-        return
-    if argv[1] != "-d":
-        print("error: " + argv[1] + " doesn't exist, try \"-d\"") 
-        return
-    if len(argv[2]) != 10:
-        print("error: incorrect date format") 
-        return
-    date = argv[2]
-    
+        return "error: file name doesn't exist"
+    if command != "-d":
+        print(f"error: {command} doesn't exist, try \"-d\"")
+        return f"error: {command} doesn't exist, try \"-d\""
+    if len(date) != 10:
+        print("error: incorrect date format")
+        return "error: incorrect date format"
+
     # opening the file
     file = open(filename)
     csvreader = csv.reader(file)
     if os.stat(filename).st_size == 0:
         print("error: csv file is empty")
-        return
+        return "error: csv file is empty"
     header = next(csvreader)
     if len(header) != 2:
         print("error: number of file headers incorrect")
-        return
+        return "error: number of file headers incorrect"
 
     # dictionary has cookie names as keys and the number of occurences
     # of that cookie as values
@@ -43,13 +38,27 @@ def main(argv):
     #find max occurences and print cookies that have that value
     if len(cookieDict) == 0:
         print("error: no data in cvs file that matches input date")
-        return
+        return "error: no data in cvs file that matches input date"
+        
+    outputString = ""
     dictVals = list(cookieDict.values())
     dictKeys = list(cookieDict.keys())
     maxDictVal = max(dictVals)
     for key in dictKeys:
         if cookieDict[key] == maxDictVal:
+            outputString += key + "\n"
             print(key)
+    return outputString
+
+def main(argv):
+    # testing for potential parameter errors
+    if len(argv) != 3:
+        print("error: parameters not sufficient")
+        return "error: parameters not sufficient"
+    filename = argv[0]
+    command = argv[1]
+    date = argv[2]
+    return most_active_cookie(filename, command, date)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
